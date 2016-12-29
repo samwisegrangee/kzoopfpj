5c6aec6a1470551b646c0ca12c2fbb717be20e43
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-12-28 19:41:48 -0500

    Narrow Look
    Narrowing the title card makes the user focus on moving down (rather
    than my normal cinematic look which spreads everything wide).
    
    Added some About copy to reward that behavior. CTA’s and other stuff to
    come.
    
    To do:
    1. Dropdown nav for mobile
    2. Calendar
    a. Styling calendar
    b. Single Column mobile calendar
    c. Semi-transparent CTA
    3. Make PayPal account and buttons for membership
    4. Make center column info responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,6 +20,21 @@ $(document).ready(function() {
   }
 
 
+  //Smooth-scrolling between links
+  $('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
   //Modal Drop Downs by Id: Make sure the <a>'s all have id="[modal tag]-open"
   //$(function() {
   //  $("[id$=-open]").click(function() {
@@ -49,4 +64,4 @@ $(document).ready(function() {
 
 });
 
-$("h1").addClass("animated bounce");
+$(".arrow").addClass("animated bounce");
