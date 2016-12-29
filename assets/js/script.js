1d49856dc8d77b75532f862e25c00038e2a8b3a5
samwisegrangee <Samwise@Samuels-MBP.netgear.com>
2016-12-29 16:58:21 -0500

    Responsive Calendar, Responsive Nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,36 +34,9 @@ $(document).ready(function() {
 	    });
 	});
 
-
-  //Modal Drop Downs by Id: Make sure the <a>'s all have id="[modal tag]-open"
-  //$(function() {
-  //  $("[id$=-open]").click(function() {
-  //    var id = this.id;
-  //    var targetId = id.replace('-open', '');
-  //    var $target = $('#' + targetId);
-  //    $($target).css("display", "block");
-  //    $($target).css("overflow-y", "auto");
-  //    $("body").css("overflow", "hidden");
-  //    $(".modal-container").css("display", "block");
-  //    $("html, body").animate({
-  //      scrollTop: 0
-  //    }, "slow"); //
-  //  });
-  //});
-
-  //$(function() {
-  //  $(".close").click(function(e) {
-  //    e.preventDefault();
-  //    $(".modal").css("display", "none");
-  //    $(".modal").css("overflow-y", "");
-  //    $("body").css("overflow", "");
-  //    window.location.hash = $("#trigger").attr("id"); //
-  //  });
-  //});
-
+  //Animations via Animate.css
+  $(".arrow").addClass("animated bounce");
+  $(".cal-icon").addClass("animated infinite pulse");
+  $(".activist").addClass("animated infinite pulse");
 
 });
-
-$(".arrow").addClass("animated bounce");
-$(".cal-icon").addClass("animated infinite pulse");
-$(".activist").addClass("animated infinite pulse");
